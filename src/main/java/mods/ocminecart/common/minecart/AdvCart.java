e4959b1c963f4d052e09a823c3a976bcb042ea9f
feldim2425 <feldim2425@gmail.com>
2015-09-13 02:21:07 +0200

    Carts are now solid and have can speed up their self

--- src/main/java/mods/ocminecart/common/minecart/AdvCart.java
+++ src/main/java/mods/ocminecart/common/minecart/AdvCart.java
@@ -1,106 +1,101 @@
 package mods.ocminecart.common.minecart;
 
+import mods.ocminecart.OCMinecart;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRailBase;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-public class AdvCart extends EntityMinecart{
-	
-	public AdvCart(World p_i1713_1_, double p_i1713_2_, double p_i1713_4_,double p_i1713_6_) {
+//Is the Base for a solid, self powered cart with a break.
+//Later I will add the Railcraft integration here
+public class AdvCart extends EntityMinecart {
+
+	protected boolean enableBreak = false; // enable break
+	protected double motor = 0.0D;
+
+	public AdvCart(World p_i1713_1_, double p_i1713_2_, double p_i1713_4_,
+			double p_i1713_6_) {
 		super(p_i1713_1_, p_i1713_2_, p_i1713_4_, p_i1713_6_);
 		this.setDisplayTileData(0);
 	}
-	
+
 	public AdvCart(World p_i1713_1) {
 		super(p_i1713_1);
 		this.setDisplayTileData(0);
 	}
-	
-	protected void entityInit(){
+
+	protected void entityInit() {
 		super.entityInit();
-		this.dataWatcher.addObject(24, new Integer(0));
+		// Freie DataWatcher 2-16, 23-32
 	}
-	
+
 	@Override
-	public void killMinecart(DamageSource p_94095_1_){
+	public void killMinecart(DamageSource p_94095_1_) {
 		this.setDead();
-        ItemStack itemstack = this.getCartItem();
+		ItemStack itemstack = this.getCartItem();
 
-        if (this.func_95999_t() != null)
-        {
-            itemstack.setStackDisplayName(this.func_95999_t());
-        }
+		if (this.func_95999_t() != null) {
+			itemstack.setStackDisplayName(this.func_95999_t());
+		}
 
-        this.entityDropItem(itemstack, 0.0F);
+		this.entityDropItem(itemstack, 0.0F);
 	}
 
 	@Override
 	public int getMinecartType() {
 		return -1;
 	}
-	
-	public int getMode(){
-		return this.dataWatcher.getWatchableObjectInt(24);
-	}
-	
-	public void setMode(int mode){
-		this.dataWatcher.updateObject(24, mode);
-	}
-	
-	//This is just debug Code to test minecart break
-	/*public boolean interactFirst(EntityPlayer p){   
-		String msg="";
-		int m;
-		
-		m=getMode();
-		
-		++m;
-		if(m>2)m=0;
-		if(this.worldObj.isRemote){	
-			switch(m){
-			case 0:
-				msg="Set Mode to NORMAL";
-				break;
-			case 1:
-				msg="Set Mode to DRIVE";
-				break;
-			case 2:
-				msg="Set Mode to BREAK";
-				break;
-			default:
-				m=0;
-				msg="ERROR:Try Again!";
-				break;
-			}
-			p.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD+msg));
-		}
-		setMode(m);
-		return true;
-	}*/
-	
-	public boolean onRail(){
-		int x=MathHelper.floor_double(this.posX);
-		int y=MathHelper.floor_double(this.posY);
-		int z=MathHelper.floor_double(this.posZ);
+
+	public boolean onRail() {
+		int x = MathHelper.floor_double(this.posX);
+		int y = MathHelper.floor_double(this.posY);
+		int z = MathHelper.floor_double(this.posZ);
 		return BlockRailBase.func_150049_b_(this.worldObj, x, y, z);
 	}
-	
-	public void onUpdate(){
+
+	public void onUpdate() {
 		super.onUpdate();
-		if(this.motionX!=0 || this.motionZ!=0){
-			if(getMode()==2 && onRail()){
-				this.setPosition(this.lastTickPosX, this.posY, this.lastTickPosZ);	//Fix: Bug on Booster Tracks  (Reset Position)
-				this.setVelocity(0,0,0);
-			}
-		}
-		
-		
 	}
-	
-	 public boolean canBePushed(){
-		 return (getMode()!=2 || !onRail());
-	 }
+
+    protected void applyDrag() {
+        if(!this.enableBreak){
+			this.motionX *= 0.9699999785423279D;
+			this.motionY *= 0.0D;
+        	this.motionZ *= 0.9699999785423279D;
+        	
+        	if(this.motor!=0){
+        		double yaw = rotationYaw * Math.PI / 180.0;
+        		this.motionX += Math.cos(yaw) * 10;
+        		this.motionZ += Math.sin(yaw) * 10;
+        		
+        		double nMotionX = Math.min( Math.abs(this.motionX) , this.motor);
+        		double nMotionZ = Math.min( Math.abs(this.motionZ) , this.motor);
+        		
+        		if(this.motionX < 0) this.motionX = - nMotionX;
+        		else this.motionX = nMotionX;
+        		
+        		if(this.motionZ < 0) this.motionZ = - nMotionZ;
+        		else this.motionZ = nMotionZ;
+        	}
+        }
+        else if (this.enableBreak){
+        	this.motionX = 0;
+        	this.motionZ = 0;
+        	this.setPosition(this.lastTickPosX, this.posY, this.lastTickPosZ);  // Fix: Bug on Booster Tracks (Reset Position)
+        }
+    }
+
+	public AxisAlignedBB getBoundingBox() {
+		return this.getCollisionBox(this);
+	}
+
+	public boolean canBePushed() {
+		return (!this.enableBreak || !onRail());
+	}
+
+
 }
