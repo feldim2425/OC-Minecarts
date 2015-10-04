680fef64f0d49776c1d42ef01c845670fb4c9a10
feldim2425 <feldim2425@gmail.com>
2015-10-04 18:52:21 +0200

    Fixed weird dropped items

--- src/main/java/mods/ocminecart/common/component/ComputerCartController.java
+++ src/main/java/mods/ocminecart/common/component/ComputerCartController.java
@@ -360,7 +360,7 @@ public class ComputerCartController implements ManagedEnvironment{
 			 if(dstack.stackSize < 1) 
 				 this.cart.mainInventory().setInventorySlotContents(sslot, null);
 			 drop.add(dif);
-			 ItemUtil.dropItemList(drop, this.cart.world(), x, y, z);
+			 ItemUtil.dropItemList(drop, this.cart.world(), x+0.5D, y+0.5D, z+0.5D);
 			 return new Object[]{ true };
 		 }
 		 else{
