8df90c3f6c4a1823a9a6e09ccf0f18a5b10ea0a5
feldim2425 <feldim2425@gmail.com>
2015-10-09 23:04:47 +0200

    Crowbar does not longer open the computer cart gui when player not sneak

--- src/main/java/mods/ocminecart/interaction/railcraft/RailcraftUtils.java
+++ src/main/java/mods/ocminecart/interaction/railcraft/RailcraftUtils.java
@@ -8,7 +8,6 @@ public class RailcraftUtils {
 	
 	public static boolean isUsingChrowbar(EntityPlayer player){
 		if(!Loader.isModLoaded("Railcraft")) return false;
-		if(!player.isSneaking()) return false;
 		if(player.getCurrentEquippedItem()!=null && (player.inventory .getCurrentItem().getItem() instanceof IToolCrowbar))
 			return true;
 		return false;
