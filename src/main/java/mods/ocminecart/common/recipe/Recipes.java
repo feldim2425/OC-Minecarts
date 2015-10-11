49f6700aa2ae43596319a559b6eb176db9356483
feldim2425 <feldim2425@gmail.com>
2015-10-11 22:20:52 +0200

    Added crafting recipe for the Remote Module

--- src/main/java/mods/ocminecart/common/recipe/Recipes.java
+++ src/main/java/mods/ocminecart/common/recipe/Recipes.java
@@ -29,13 +29,20 @@ public class Recipes {
 		"MBM",
 		"IKI", 'S',items.get("lanCard").createItemStack(1), 'C',items.get("chamelium").createItemStack(1), 'I', new ItemStack(Items.iron_ingot),
 		       'M',items.get("chip2").createItemStack(1), 'K',Item.getItemFromBlock(items.get("cable").block()), 'B',items.get("printedCircuitBoard").createItemStack(1));
-	
+		
+		GameRegistry.addShapedRecipe(new ItemStack(ModItems.item_CartRemoteModule,1,0),
+				"IC0",
+				"IBR",
+				"IGW", 'I',items.get("nuggetIron").createItemStack(1), 'C',items.get("chip2").createItemStack(1), 'B', items.get("printedCircuitBoard").createItemStack(1), 'R', Items.redstone,
+					   'G',items.get("generatorUpgrade").createItemStack(1),'W',items.get("wlanCard").createItemStack(1));
+		
 	
 		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.block_NetworkRail ), Item.getItemFromBlock(items.get("cable").block()), new ItemStack(Items.redstone), Item.getItemFromBlock(Blocks.detector_rail));
 	
 		GameRegistry.addShapedRecipe(new ItemStack(ModItems.item_ComputerCartCase,1,0),"X","Y","Z",'X',Item.getItemFromBlock(items.get("case1").block()), 'Y', Items.minecart, 'Z', Item.getItemFromBlock(items.get("cable").block()));
 		GameRegistry.addShapedRecipe(new ItemStack(ModItems.item_ComputerCartCase,1,1),"X","Y","Z",'X',Item.getItemFromBlock(items.get("case2").block()), 'Y', Items.minecart, 'Z', Item.getItemFromBlock(items.get("cable").block()));
 		GameRegistry.addShapedRecipe(new ItemStack(ModItems.item_ComputerCartCase,1,2),"X","Y","Z",'X',Item.getItemFromBlock(items.get("case3").block()), 'Y', Items.minecart, 'Z', Item.getItemFromBlock(items.get("cable").block()));
+		
 		
 		GameRegistry.addRecipe(new RomCrafting(new ItemStack(ModItems.item_ComputerCart) , ItemComputerCart.SLOT_ROM));
 		
