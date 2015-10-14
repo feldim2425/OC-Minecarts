6fffc4702dd5d5f388afa28c3e09248541ea50a1
feldim2425 <feldim2425@gmail.com>
2015-10-14 20:20:41 +0200

    Updated the Railcraft API

--- src/main/java/mods/ocminecart/OCMinecart.java
+++ src/main/java/mods/ocminecart/OCMinecart.java
@@ -19,7 +19,9 @@ import cpw.mods.fml.common.event.FMLInterModComms.IMCEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = OCMinecart.MODID, name=OCMinecart.NAME, version=OCMinecart.VERSION, dependencies = "required-after:OpenComputers@[1.5.14,); after:Waile@[1.5.8a,)")
+@Mod(modid = OCMinecart.MODID, name=OCMinecart.NAME, version=OCMinecart.VERSION, dependencies = "required-after:OpenComputers@[1.5.14,);"
+		+ "after:Waile@[1.5.8a,);"
+		+ "after:Railcraft@[9.7.0.0,)")
 public class OCMinecart {
 	public static final String MODID = "ocminecart";
 	public static final String VERSION = "0.5-alpha";
