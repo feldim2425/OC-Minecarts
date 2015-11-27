2e9cfb61aa4b0bfdc23a9d8647c71ad8ad828300
feldim2425 <feldim2425@gmail.com>
2015-11-27 19:23:08 +0100

    Fixed the ComputerCart render yaw

--- src/main/java/mods/ocminecart/client/renderer/entity/ComputerCartRenderer.java
+++ src/main/java/mods/ocminecart/client/renderer/entity/ComputerCartRenderer.java
@@ -22,61 +22,48 @@ public class ComputerCartRenderer extends Render {
 		
 		GL11.glPushMatrix();
         this.bindEntityTexture(cart);
-        long i = (long)cart.getEntityId() * 493286711L;
-        i = i * i * 4392167121L + i * 98761L;
-        float f2 = (((float)(i >> 16 & 7L) + 0.5F) / 8.0F - 0.5F) * 0.004F;
-        float f3 = (((float)(i >> 20 & 7L) + 0.5F) / 8.0F - 0.5F) * 0.004F;
-        float f4 = (((float)(i >> 24 & 7L) + 0.5F) / 8.0F - 0.5F) * 0.004F;
-        GL11.glTranslatef(f2, f3, f4);
-        double d3 = cart.lastTickPosX + (cart.posX - cart.lastTickPosX) * (double)p_76986_9_;
-        double d4 = cart.lastTickPosY + (cart.posY - cart.lastTickPosY) * (double)p_76986_9_;
-        double d5 = cart.lastTickPosZ + (cart.posZ - cart.lastTickPosZ) * (double)p_76986_9_;
+        
+        double cx= cart.lastTickPosX + (cart.posX - cart.lastTickPosX) * (double)p_76986_9_;
+        double cy= cart.lastTickPosY + (cart.posY - cart.lastTickPosY) * (double)p_76986_9_;
+        double cz= cart.lastTickPosZ + (cart.posZ - cart.lastTickPosZ) * (double)p_76986_9_;
         double d6 = 0.30000001192092896D;
-        Vec3 vec3 = cart.func_70489_a(d3, d4, d5);
-        float f5 = cart.prevRotationPitch + (cart.rotationPitch - cart.prevRotationPitch) * p_76986_9_;
-
-        if (vec3 != null)
-        {
-            Vec3 vec31 = cart.func_70495_a(d3, d4, d5, d6);
-            Vec3 vec32 = cart.func_70495_a(d3, d4, d5, -d6);
-
-            if (vec31 == null)
-            {
-                vec31 = vec3;
-            }
-
-            if (vec32 == null)
-            {
-                vec32 = vec3;
-            }
-
-            x += vec3.xCoord - d3;
-            y += (vec31.yCoord + vec32.yCoord) / 2.0D - d4;
-            z += vec3.zCoord - d5;
-            Vec3 vec33 = vec32.addVector(-vec31.xCoord, -vec31.yCoord, -vec31.zCoord);
-
-            if (vec33.lengthVector() != 0.0D)
-            {
-                vec33 = vec33.normalize();
-                p_76986_8_ = (float)(Math.atan2(vec33.zCoord, vec33.xCoord) * 180.0D / Math.PI);
-                f5 = (float)(Math.atan(vec33.yCoord) * 73.0D);
-            }
+        double ryaw = (cart.rotationYaw+360D)%360;
+        
+        double yaw=p_76986_8_;
+        float pitch = cart.rotationPitch;
+        
+        Vec3 vec1 = cart.func_70495_a(cx,cy,cz, d6);
+        Vec3 vec2 = cart.func_70495_a(cx,cy,cz, -d6);
+        
+        if(vec1!=null && vec2!=null){
+             y += (vec1.yCoord + vec2.yCoord) / 2.0D - cy;
+             Vec3 vec3 = vec2.addVector(-vec1.xCoord, -vec1.yCoord, -vec1.zCoord);
+             if(vec3.lengthVector()!=0){
+            	 yaw = (float)(Math.atan2(vec3.zCoord, vec3.xCoord) * 180.0D / Math.PI);
+            	 pitch = (float)(Math.atan(vec3.yCoord) * 73.0D);
+             }
         }
-
+        
+        yaw=(yaw+360D)%360D;
+        ryaw=yaw-ryaw;
+        if(ryaw<=-90 || ryaw>=90){
+        	yaw+=180D;
+        	pitch*=-1;
+        }
+        yaw=90F-yaw;
+        
         GL11.glTranslatef((float)x, (float)y, (float)z);
-        GL11.glRotatef(90.0F - p_76986_8_,0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-f5, 1.0F, 0.0F, 0.0F);
-        float f7 = (float)cart.getRollingAmplitude() - p_76986_9_;
-        float f8 = cart.getDamage() - p_76986_9_;
+        GL11.glRotated(yaw, 0.0D, 1.0D, 0.0D);
+        GL11.glRotatef(-pitch, 1.0F, 0.0F, 0.0F);
+        float rollamp = (float)cart.getRollingAmplitude() - p_76986_9_;
+        float dmgamp = cart.getDamage() - p_76986_9_;
+        
+        if (dmgamp < 0.0F)
+        	dmgamp = 0.0F;
 
-        if (f8 < 0.0F)
+        if (rollamp > 0.0F)
         {
-            f8 = 0.0F;
-        }
-
-        if (f7 > 0.0F)
-        {
-            GL11.glRotatef(MathHelper.sin(f7) * f7 * f8 / 10.0F * (float)cart.getRollingDirection(), 0.0F, 0.0F, 1.0F);
+            GL11.glRotatef(MathHelper.sin(rollamp) * rollamp * dmgamp / 10.0F * (float)cart.getRollingDirection(), 0.0F, 0.0F, 1.0F);
         }
         
         GL11.glColor3f(1, 1, 1);
