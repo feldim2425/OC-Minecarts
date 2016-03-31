98f77d3bd8a8de9f96ea6d6388c1a22efd9b7a09
feldim2425 <feldim2425@gmail.com>
2016-03-31 19:55:39 +0200

    New Version

--- src/main/java/mods/ocminecart/OCMinecart.java
+++ src/main/java/mods/ocminecart/OCMinecart.java
@@ -23,7 +23,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 		+ "after:Railcraft@[9.7.0.0,)")
 public class OCMinecart {
 	public static final String MODID = "ocminecart";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.2.1";
 	public static final String NAME = "OC-Minecarts";
 	
 	@Instance(OCMinecart.MODID)
