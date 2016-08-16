de70d6ce9fb9af35a084c395ccb62757a349bdb9
feldim2425 <feldim2425@gmail.com>
2016-08-16 18:07:45 +0200

    Updated Version (1.7)

--- src/main/java/mods/ocminecart/OCMinecart.java
+++ src/main/java/mods/ocminecart/OCMinecart.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.Logger;
 		+ "after:Railcraft@[9.7.0.0,)")
 public class OCMinecart {
 	public static final String MODID = "ocminecart";
-	public static final String VERSION = "1.6";
+	public static final String VERSION = "1.7";
 	public static final String NAME = "OC-Minecarts";
 	
 	@Instance(OCMinecart.MODID)
